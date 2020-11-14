75a09ac6bb0e003d700901463cd8f1821df06f71
XaverOz <xaverozz@gmail.com>
2020-11-14 12:40:40 +0500

    spring MVC

--- src/main/java/com/example/demo/controller/RestCarController.java
+++ src/main/java/com/example/demo/controller/RestCarController.java
@@ -4,35 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Car;
 
-@Controller
-public class CarController {
-	
+@RestController
+@RequestMapping("/")
+public class RestCarController {
 	@Autowired
 	Car car;
-
-
-	@GetMapping("/cars")
-    public String listCars(Model model) {
+	
+	@RequestMapping(value = "/rest/list-cars", method = RequestMethod.GET)
+    public List<Car> pageVisit() {
 		car.setColor("red");
 		car.setName("LADA2110");
 		List<Car> cars = new ArrayList<>();
 		cars.add(car);
 		cars.add(car);
-		model.addAttribute("cars", cars);
-		// имя шаблона Themyleaf
-		return "cars"; 
+		return cars;
 	}
 	
-	@GetMapping("/cars-front")
-    public String listCars() {
-		return "cars-front"; 
-	}
 }
---
+++ src/main/resources/static/script.js
@@ -0,0 +1,20 @@
+function sendAjax() {
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', './rest/list-cars', false);
+	xhr.onreadystatechange = function() { 
+		  if (xhr.readyState != 4) {
+			  return;
+		  } else if(xhr.status == 200)  {
+			  var cars = JSON.parse(xhr.responseText);
+			  var carsElement = document.getElementById('cars');
+			  for(let car of cars) {
+				  var carElement = document.createElement('div');
+				  carElement.innerHTML = car.name;
+				  carsElement.appendChild(carElement); 
+			  }
+		  }
+	}
+	xhr.send();
+}
+
+window.onload = sendAjax;
