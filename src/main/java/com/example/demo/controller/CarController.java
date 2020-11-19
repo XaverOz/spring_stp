1ab18ef7cb56adc8006c6d3bafc11e2c2959f0f4
XaverOz <xaverozz@gmail.com>
2020-11-19 11:10:18 +0500

    add Spring Secirity

--- src/main/java/com/example/demo/controller/CarController.java
+++ src/main/java/com/example/demo/controller/CarController.java
@@ -1,12 +1,28 @@
 package com.example.demo.controller;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.util.UriComponentsBuilder;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -21,14 +37,41 @@ public class CarController {
 
 	@GetMapping("/cars")
     public String listCars(Model model) {
-		car.setColor("red");
-		car.setName("LADA2110");
 		List<Car> cars = new ArrayList<>();
-		cars.add(car);
-		cars.add(car);
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader ("cars"));
+			String         line = null;
+		    while((line = reader.readLine()) != null) {
+		    	Car car = new Car();
+		    	car.setName(line);
+	            cars.add(car);
+	        }
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		model.addAttribute("cars", cars);
 		// имя шаблона Themyleaf
 		return "cars"; 
+	}
+	
+	@GetMapping("/add-car")
+    public String addCarView(Model model) {
+		return "car-add"; 
+	}
+	
+	@PostMapping(value = "/add-car")
+    public RedirectView addCar(HttpServletRequest request) {
+		BufferedWriter writer = null;
+		try {
+			writer = new BufferedWriter(new FileWriter("cars", true));
+			writer.write(request.getParameter("car") + "\n");
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return new RedirectView("cars");
 	}
 	
 	@GetMapping("/cars-front")
--- src/main/java/com/example/demo/controller/RestCarController.java
+++ src/main/java/com/example/demo/controller/RestCarController.java
@@ -1,10 +1,13 @@
 package com.example.demo.controller;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,13 +21,23 @@ public class RestCarController {
 	@Autowired
 	Car car;
 	
+	@CrossOrigin(origins = "http://127.0.0.1:8000")
 	@RequestMapping(value = "/rest/list-cars", method = RequestMethod.GET)
     public List<Car> pageVisit() {
-		car.setColor("red");
-		car.setName("LADA2110");
 		List<Car> cars = new ArrayList<>();
-		cars.add(car);
-		cars.add(car);
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader ("cars"));
+			String         line = null;
+		    while((line = reader.readLine()) != null) {
+		    	Car car = new Car();
+		    	car.setName(line);
+	            cars.add(car);
+	        }
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return cars;
 	}
 	
